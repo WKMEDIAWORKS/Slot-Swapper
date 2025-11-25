e6c65aed8d5041b615e9735e5a69a61bd5e640de
waris <waris@wariss-MacBook-Air.local>
2025-11-25 19:57:17 +0530

    Update server.js, switch to Pool and fix DB handling

--- server.js
+++ server.js
@@ -21,15 +21,21 @@ app.use(cookieParser());
 
 
 
-const db = new pg.Client({
+const { Pool } = pg;
+
+ const db = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false,
     },
+    max: 10,
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 5000,
 });
 
-db.connect();
-
+db.on("error", (err) => {
+    console.error("Unexpected PG error from idle client: ", err);
+});
 
 app.set("views", "./public/views");
 app.set("view engine", "ejs");
